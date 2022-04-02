3a50c95c81e3ec8ef07eead16723705f7019f27f
Nick <s6ninord@uni-bonn.de>
2022-04-02 11:54:42 +0200

    erst vergessen

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-    int x;
-    int sum;
+    string name;
+    cout << "Name des Graphen?" << endl;
+    cin >> name;
 
-    printf("Hi");
+    int a;
+
+    ifstream myFile(name.append(".txt"));
+
+    if(myFile.is_open()) {
+        printf("The filed was opened correctly\n");
+    }
+    else{
+        printf("Error: opening the file failed\n");
+        exit(1);
+    }
+
+    while(myFile >> a) {
+        printf("%d\n",a);
+    }
+
+    myFile.close();
+
     return 0;
 }
